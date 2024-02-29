b4717e6b79798858dbdc657a981264efe15595e4
josimara-lima2 <josimara_lima88@hotmail.com>
2024-02-29 20:47:29 -0300

    desafios 29/02

--- matematica/desafio-1457.js
+++ matematica/desafio-1457.js
@@ -1,6 +1,8 @@
 var input = require('fs').readFileSync('stdin', 'utf8');
 var lines = input.split('\n');
 
+
+
 var qtdCasos = Number(lines.shift())
 function oracleAlexandria(n,k){
     var total = BigInt(n)
